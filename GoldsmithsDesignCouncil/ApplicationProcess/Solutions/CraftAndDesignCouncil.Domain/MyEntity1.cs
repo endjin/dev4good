c9cc0101d79a05de031e2369891a25a65059b62b
Joe Simmonds <joe.simmonds@gmail.com>
2011-07-03 13:12:29 +0100

    initial commit of sharp arch based application process

---
+++ GoldsmithsDesignCouncil/ApplicationProcess/Solutions/CraftAndDesignCouncil.Domain/MyEntity1.cs
@@ -0,0 +1,8 @@
+﻿namespace CraftAndDesignCouncil.Domain
+{
+    using SharpArch.Domain.DomainModel;
+
+    public class MyEntity1 : Entity
+    {
+    }
+}
---
+++ GoldsmithsDesignCouncil/ApplicationProcess/Solutions/CraftAndDesignCouncil.Web.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace CraftAndDesignCouncil.Web.Mvc.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
