96207f2e647ea6761c5848dba9d245c689d36d85
Howard.vanRooijen <HowardvanRooijen@gmail.com>
2011-07-03 15:15:31 +0100

    .

---
+++ GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Data/Schema Objects/Database Level Objects/Storage/Files/CADC_AwardsProcess_log.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD LOG FILE (NAME = [CADC_AwardsProcess_log], FILENAME = '$(DefaultLogPath)CADC_AwardsProcess_log.ldf', MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
+
