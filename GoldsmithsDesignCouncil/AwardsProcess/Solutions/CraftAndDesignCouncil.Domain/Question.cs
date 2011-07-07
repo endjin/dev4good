37fbc65a3e03aa1927fa1a9d26ecc22b0c8fbb1c
Joe Simmonds <joe.simmonds@gmail.com>
2011-07-07 13:30:37 +0100

    moved page header and footer to layout page. added Question entity (not used yet) added Admin page (nothing in it yet)

---
+++ GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Domain/Question.cs
@@ -0,0 +1,16 @@
+﻿namespace CraftAndDesignCouncil.Domain
+{
+    #region Using Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using SharpArch.Domain.DomainModel;
+    #endregion
+
+    public class Question : Entity
+    {
+        public virtual string QuestionText { get; set; }
+        public virtual string HelpText { get; set; }
+    }
+}
---
+++ GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Web.Mvc/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CraftAndDesignCouncil.Web.Mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
