e732f4a3c3f90a9407595ac06fe3926c8def6d93
Joe Simmonds <joe.simmonds@gmail.com>
2011-07-29 22:27:13 +0100

    Added formsection entities

---
+++ GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Data/Schema Objects/Schemas/dbo/Tables/ApplicationFormSection.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ApplicationFormSection]
+(
+	ApplicationFormSectionId int NOT NULL identity (1,1) primary key, 
+	Title NVarChar(255) not null,
+	OrderingKey int not null,
+	NotRequiredIfQuestionId int,
+	NotRequiredIfAnswer NVarchar(255)
+)
--- GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Domain/ApplicationFormSection.cs
+++ GoldsmithsDesignCouncil/AwardsProcess/Solutions/CraftAndDesignCouncil.Domain/ApplicationFormSection.cs
@@ -7,6 +7,10 @@ namespace CraftAndDesignCouncil.Domain
 
     public class ApplicationFormSection : Entity
     {
+       public virtual string Title { get; set; }
+       public virtual int OrderingKey { get; set; }
        public virtual IList<QuestionAnswer> Questions { get; set; }
+       public virtual Question NotRequiredIfQuestion { get; set; }
+       public virtual string NotRequiredIfAnswer { get; set; }
     }
 }
